ef57bbef2d8fc0ecaf3f1127f9c14c25c2216d9a
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2025-01-12 18:13:16 +0100

    Implement Sign Up API in NestJS with Password Hashing

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { PrismaService } from './prisma/prisma/prisma.service';
-import { PrismaService } from './prisma/prisma.service';
+
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
